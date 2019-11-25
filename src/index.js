7bac2f866f317bf82e4d477b743076214448c7e6
anagumait <tdkrbear801@gmail.com>
2019-11-25 23:09:59 +0900

    css追加とvueの追加

--- src/index.js
+++ src/index.js
@@ -1,6 +1,69 @@
 var app = new Vue({
     el: '#app',
     data: {
-        message: 'Hello Vue!'
+        message: 'Hello Vue!',
+        //家計簿明細
+        houseHoldD: [
+            {
+                date: '2019/11/23',
+                weekNum: '3',
+                account: '食費',
+                detail: 'パン',
+                income: '0',
+                spending: '100',
+                balance: '10000',
+                modeOfPay: '現金',
+                owner: 'みどりの',
+                conWasInv: '消費'
+            },
+            {
+                date: '2019/11/23',
+                weekNum: '3',
+                account: '食費',
+                detail: 'パン',
+                income: '0',
+                spending: '100',
+                balance: '10000',
+                modeOfPay: '現金',
+                owner: 'みどりの',
+                conWasInv: '消費'
+            },
+            {
+                date: '2019/11/23',
+                weekNum: '3',
+                account: '食費',
+                detail: 'パン',
+                income: '0',
+                spending: '100',
+                balance: '10000',
+                modeOfPay: '現金',
+                owner: 'みどりの',
+                conWasInv: '消費'
+            },
+            {
+                date: '2019/11/23',
+                weekNum: '3',
+                account: '食費',
+                detail: 'パン',
+                income: '0',
+                spending: '100',
+                balance: '10000',
+                modeOfPay: '現金',
+                owner: 'みどりの',
+                conWasInv: '消費'
+            },
+            {
+                date: '2019/11/23',
+                weekNum: '3',
+                account: '食費',
+                detail: 'パン',
+                income: '0',
+                spending: '100',
+                balance: '10000',
+                modeOfPay: '現金',
+                owner: 'みどりの',
+                conWasInv: '消費'
+            },
+        ]
     }
 });

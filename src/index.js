d4806d079508c61aea45d110700f3dc5f87c6b19
anagumait <tdkrbear801@gmail.com>
2019-11-26 21:46:37 +0900

    入力フォーム（簡易版）実装

--- src/index.js
+++ src/index.js
@@ -64,5 +64,33 @@ var app = new Vue({
                 conWasInv: '消費'
             }
         ]
-    }
+    },
+    methods: {
+        //リストへの追加
+        doAdd: function(event, value) {
+          // ref で名前を付けておいた要素を参照
+          var date = this.$refs.date
+          // 入力がなければ何もしないで return
+          if (!date.value.length) {
+            return
+          }
+          // { 新しいID, コメント, 作業状態 }
+          // というオブジェクトを現在の todos リストへ push
+          // 作業状態「state」はデフォルト「作業中=0」で作成
+          this.houseHoldD.push({
+            date: date.value,
+            weekNum: '3',
+            account: '食費',
+            detail: 'パン',
+            income: '0',
+            spending: '100',
+            balance: '10000',
+            modeOfPay: '現金',
+            owner: 'みどりの',
+            conWasInv: '消費'
+          })
+          // フォーム要素を空にする
+          date.value = ''
+        }
+      }
 });
